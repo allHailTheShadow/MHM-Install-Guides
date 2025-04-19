c4547d50fcd5f2878a2d99bd3d625c3ee6f538ba
allHailTheShadow <90817120+allHailTheShadow@users.noreply.github.com>
2025-04-19 17:44:09 +1000

    Update AIO-to-individual-MO2-files.ps1

--- AIO-to-individual-MO2-files.ps1
+++ AIO-to-individual-MO2-files.ps1
@@ -1,116 +1,171 @@
-#######################################################################################
+##############################################################
 #
 # INSTRUCTIONS:
-#     Must have 7zip installed to "C:\Program Files\7-Zip"
-#     Create new folder
-#     Add this file to the folder
-#     Add "A. Modular Hard Mode - AIO-88-<whatever>.7z" to the same folder as this file
-#     Right click this file and select run with powershell
+#     1. Set your customizations in the next section
+#     2. Create a new folder
+#     3. Download the MHM AIO file to this folder
+#     4. Add this file to the folder
+#     5. Right click on this file, and select run with powershell
 #
-#######################################################################################
+##############################################################
 
 
+
+###############################################################################################################
+# CUSTOMIZE THIS SECTION TO YOUR LIKING
 #
-# Variables
+# These options will send the pak files into the mod named on the right
+#     $aio_hardcore = 'MHM - AIO - Hardcore'
+#     Sends the pak file "zzzzzz_MHM_2.0_AIO_Full_Hardcore.pak" into a mod called 'MHM - AIO Hardcore'
+# 
 #
-$MOD_ID  = '88'
-$VERSION = '2-0-8G'
-$OTHER_THING = '1743824818'
+# To send for example both of the 'Found a Way' patches into a single mod, just set them to have the same name:
+#     $patches_faw_regular  = 'MHM - Patch - A-Life Found a Way'
+#     $patches_faw_master   = 'MHM - Patch - A-Life Found a Way'
+#
+# Only things in this section should need changing, leave everything else alone.
+#
+###############################################################################################################
+
+$7z_dir               = 'C:\Program Files\7-Zip'
+$aio_hardcore         = 'MHM - AIO - Hardcore'
+$aio_standard         = 'MHM - AIO - Standard'
+$localization_vanilla = 'MHM - Localization - Vanilla'
+$localization_rwng    = 'MHM - Localization - RWNG'
+$textures             = 'MHM - Textures'
+$interface            = 'MHM - Interface'
+$addon_gamepad        = 'MHM - Addon - Gamepad'
+$addon_master_mode    = 'MHM - Addon - Master Mode'
+$addon_campfire_saves = 'MHM - Addon - Campfire Saves'
+$addon_reshade        = 'MHM - Addon - Reshade - Color Fix'
+$addon_reshade_gloom  = 'MHM - Addon - Reshade - Gloom'
+$addon_weather        = 'MHM - Addon - Weather'
+$addon_relaxed_mode   = 'MHM - Addon - Relaxed Mode'
+$patches_faw_regular  = 'MHM - Patch - A-Life Found a Way - Regular'
+$patches_faw_master   = 'MHM - Patch - A-Life Found a Way - Master Mode'
+$patches_potz         = 'MHM - Patch - Painter of the Zone'
+$patches_svarog       = 'MHM - Patch - Svarog Detector'
 
 
+
+############################################
 #
-# Extract AIO
+# extract mod id, version, etc from filename
 #
-& "C:\Program Files\7-Zip\7z.exe" x '.\A. Modular Hard Mode - AIO-88-*7z'
+############################################
+
+'.\A. Modular Hard Mode - AIO-88-2-0-83Q-1745033351.7z' -match "^.*AIO-(\d\d)-(.*)-(.*).7z$"
+$mod_id               = $matches[1]
+$version              = $matches[2]
+$other_thing          = $matches[3]
 
 
+
+##########################
 #
-# Make directories for the output
+# extract the AIO zip file
 #
-mkdir '.\MHM-AIO-Hardcore\Content\Paks\~mods'
-mkdir '.\MHM-AIO-Standard\Content\Paks\~mods'
-mkdir '.\MHM-Localization\Content\Paks\~mods'
-mkdir '.\MHM-Interface\Content\Paks\~mods'
-mkdir '.\MHM-Textures\Content\Paks\~mods'
-mkdir '.\MHM-Addon-Weather\Content\Paks\~mods'
-mkdir '.\MHM-Addon-Gamepad\Content\Paks\~mods'
-mkdir '.\MHM-Addon-MasterMode\Content\Paks\~mods'
-mkdir '.\MHM-Addon-Campfire\Content\Paks\~mods'
-mkdir '.\MHM-Addon-Reshade\Binaries\Win64'
-mkdir '.\MHM-Addon-Relaxed\Content\Paks\~mods'
-mkdir '.\MHM-Patch-FAW\Content\Paks\~mods'
-mkdir '.\MHM-Patch-FAW-MasterMode\Content\Paks\~mods'
-mkdir '.\MHM-Patch-PotZ\Content\Paks\~mods'
-mkdir '.\MHM-Patch-Svarog\Content\Paks\~mods'
+##########################
+
+Write-Host "Extracting AIO zip... " -NoNewLine
+& "${7z_dir}\7z.exe" x '.\A. Modular Hard Mode - AIO*7z' > $null
+Write-Host "Done!"
 
 
+
+##################################
 #
-# Move files to appropriate folders
+# make folders for individual mods
 #
-mv '.\zzzzzzz_MHM_Interface0z_P.*'            '.\MHM-Interface\Content\Paks\~mods'
-mv '.\zzzzzzz_MHMTextures0z_P.*'              '.\MHM-Textures\Content\Paks\~mods'
-mv '.\A. Hardcore AIO\*.pak'                  '.\MHM-AIO-Hardcore\Content\Paks\~mods'
-mv '.\B. Standard AIO\*.pak'                  '.\MHM-AIO-Standard\Content\Paks\~mods'
-mv '.\Localization*\Real*\LocalizationDB_P.*' '.\MHM-Localization\Content\Paks\~mods'
-mv '.\*Addons*\*Weather*\*pak'                '.\MHM-Addon-Weather\Content\Paks\~mods'
-mv '.\*Addons*\*Gamepad*\*pak'                '.\MHM-Addon-Gamepad\Content\Paks\~mods'
-mv '.\*Addons*\*Master*\*pak'                 '.\MHM-Addon-MasterMode\Content\Paks\~mods'
-mv '.\*Addons*\*Campfire*\*pak'               '.\MHM-Addon-Campfire\Content\Paks\~mods'
-mv '.\*Addons*\*Color*\MHM.ini'               '.\MHM-Addon-Reshade\Binaries\Win64'
-mv '.\*Addons*\*Relaxed*\*pak'                '.\MHM-Addon-Relaxed\Content\Paks\~mods'
-mv '.\*Patches*\*Found*Way*\*MasterMode*pak'  '.\MHM-Patch-FAW-MasterMode\Content\Paks\~mods'
-mv '.\*Patches*\*Found*Way*\*pak'             '.\MHM-Patch-FAW\Content\Paks\~mods'
-mv '.\*Patches*\*Painter*\*pak'               '.\MHM-Patch-PotZ\Content\Paks\~mods'
-mv '.\*Patches*\*Svarog*\*pak'                '.\MHM-Patch-Svarog\Content\Paks\~mods'
+##################################
+
+$mods_standard = @(
+	$aio_hardcore, 
+	$aio_standard, 
+	$localization_vanilla, 
+	$localization_rwng, 
+	$textures, 
+	$interface, 
+	$addon_gamepad, 
+	$addon_master_mode, 
+	$addon_campfire_saves, 
+	$addon_weather, 
+	$addon_relaxed_mode, 
+	$patches_faw_regular, 
+	$patches_faw_master, 
+	$patches_potz, 
+	$patches_svarog
+)
+$mods_special = @(
+	$addon_reshade, 
+	$addon_reshade_gloom
+)
+$mods_all = $mods_standard + $mods_special
+$folders_standard = '\Content\Paks\~mods'
+$folders_special  = '\Binaries\Win64'
+Write-Host "Creating folders... " -NoNewLine
+foreach ($folder in $mods_standard) { mkdir ".\${folder}${folders_standard}" -Force > $null }
+foreach ($folder in $mods_special)  { mkdir ".\${folder}${folders_special}"  -Force > $null }
+Write-Host "Done!"
 
 
+
+#######################################
 #
-# Remove leftovers
+# move extracted files into mod folders
 #
-rm '.\A. Hardcore*'  -r -Force
-rm '.\B. Standard*'  -r -Force
-rm '.\Localization*' -r -Force
-rm '.\Patches*'      -r -Force
-rm '.\*pak'
-rm '.\*ucas'
-rm '.\*utoc'
+#######################################
+
+Write-Host "Moving extracted files into folders... " -NoNewLine
+mv '.\A. Hardcore AIO\*.pak'                     ".\${aio_hardcore}${folders_standard}" > $null
+mv '.\B. Standard AIO\*.pak'                     ".\${aio_standard}${folders_standard}" > $null
+mv '.\localization*\Real*\localizationDB_P.*'    ".\${localization_rwng}${folders_standard}" > $null
+mv '.\localization*\Vanilla*\localizationDB_P.*' ".\${localization_vanilla}${folders_standard}" > $null
+mv '.\zzzzzzz_MHMtextures0z_P.*'                 ".\${textures}${folders_standard}" > $null
+mv '.\zzzzzzz_MHM_interface0z_P.*'               ".\${interface}${folders_standard}" > $null
+mv '.\*Addons*\*Gamepad*\*pak'                   ".\${addon_gamepad}${folders_standard}" > $null
+mv '.\*Addons*\*Master*\*pak'                    ".\${addon_master_mode}${folders_standard}" > $null
+mv '.\*Addons*\*Campfire*\*pak'                  ".\${addon_campfire_saves}${folders_standard}" > $null
+mv '.\*Addons*\*Weather*\*pak'                   ".\${addon_weather}${folders_standard}" > $null
+mv '.\*Addons*\*Relaxed*\*pak'                   ".\${addon_relaxed_mode}${folders_standard}" > $null
+mv '.\*Patches*\*Found*Way*\*MasterMode*pak'     ".\${patches_faw_master}${folders_standard}" > $null
+mv '.\*Patches*\*Found*Way*\*pak'                ".\${patches_faw_regular}${folders_standard}" > $null
+mv '.\*Patches*\*Painter*\*pak'                  ".\${patches_potz}${folders_standard}" > $null
+mv '.\*Patches*\*Svarog*\*pak'                   ".\${patches_svarog}${folders_standard}" > $null
+mv '.\*Addons*\*Color*\MHM.ini'                  ".\${addon_reshade}${folders_special}" > $null
+mv '.\*Addons*\*Color*\MHMgloom.ini'             ".\${addon_reshade_gloom}${folders_special}" > $null
+Write-Host "Done!"
 
 
+
+##################################
 #
-# Move folders into archives
+# delete the extracted directories
 #
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM AIO - Hardcore-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"         '.\MHM-AIO-Hardcore\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM AIO - Standard-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"         '.\MHM-AIO-Standard\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Localization-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"           '.\MHM-Localization\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Interface-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"              '.\MHM-Interface\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Textures-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"               '.\MHM-Textures\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - Weather-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"        '.\MHM-Addon-Weather\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - Gamepad-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"        '.\MHM-Addon-Gamepad\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - MasterMode-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"     '.\MHM-Addon-MasterMode\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - Campfire-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"       '.\MHM-Addon-Campfire\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - Reshade-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"        '.\MHM-Addon-Reshade\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Addon - Relaxed-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"        '.\MHM-Addon-Relaxed\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Patch - FAW-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"            '.\MHM-Patch-FAW\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Patch - FAW MasterMode-${MOD_ID}-${VERSION}-${OTHER_THING}.7z" '.\MHM-Patch-FAW-MasterMode\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Patch - PotZ-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"           '.\MHM-Patch-PotZ\*' -sdel
-& "C:\Program Files\7-Zip\7z.exe" a ".\MHM Patch - Svarog-${MOD_ID}-${VERSION}-${OTHER_THING}.7z"         '.\MHM-Patch-Svarog\*' -sdel
+##################################
+
+Write-Host "Deleting extracted directories... " -NoNewLine
+rm '.\A. Hardcore*'  -r -Force > $null
+rm '.\B. Standard*'  -r -Force > $null
+rm '.\localization*Required*' -r -Force > $null
+rm '.\Patches*'      -r -Force > $null
+Write-Host "Done!"
 
 
+
+######################################################
 #
-# Remove now empty directories
+# zip up the mods into archives and delete the folders
 #
-rm '.\MHM-AIO-Hardcore'          -r -Force
-rm '.\MHM-AIO-Standard'          -r -Force
-rm '.\MHM-Localization'          -r -Force
-rm '.\MHM-Interface'             -r -Force
-rm '.\MHM-Textures'              -r -Force
-rm '.\MHM-Addon-Weather'         -r -Force
-rm '.\MHM-Addon-Gamepad'         -r -Force
-rm '.\MHM-Addon-MasterMode'      -r -Force
-rm '.\MHM-Addon-Campfire'        -r -Force
-rm '.\MHM-Addon-Reshade'         -r -Force
-rm '.\MHM-Addon-Relaxed'         -r -Force
-rm '.\MHM-Patch-FAW'             -r -Force
-rm '.\MHM-Patch-FAW-MasterMode'  -r -Force
-rm '.\MHM-Patch-PotZ'            -r -Force
-rm '.\MHM-Patch-Svarog'          -r -Force
+######################################################
+
+Write-Host "Creating mod archives..."
+foreach ($mod in $mods_all)
+{
+	if (Test-Path -Path $mod) {
+		Write-Host "    ${mod}... " -NoNewLine
+		& "${7z_dir}\7z.exe" a "${mod}-${mod_id}-${version}-${other_thing}.7z" ".\${mod}\*" -sdel > $null
+		rm ".\${mod}" -r -Force > $null
+		Write-Host "Done!"
+	}
+}
